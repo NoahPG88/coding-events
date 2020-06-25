4f717707c8cf2b073551dc9accb7eb9c43dc9a2b
NoahPG88 <57874889+NoahPG88@users.noreply.github.com>
2020-06-25 16:25:38 -0500

    not sure where we left off... but time to branch for ORM

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,6 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 public class Event {
@@ -7,14 +8,66 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 50, message = "Name must be between 3-50 chars")
     private String name;
+
+    @NotBlank(message = "Email required")
+    @Email(message = "Invalid email.  Try again.")
+    private String contactEmail;
+
+    private boolean required;
+
+    @Max(value = 10000, message = "10,000 attendee maximum")
+    @Min(value = 10, message = "10 attendee minimum")
+    private Integer attendees;
+
+    @Size(min = 3, max = 50, message = "Location must be between 3-50 chars")
+    @NotBlank(message = "Location is required")
+    private String location;
+
+    @Size(max = 750, message = "Description too long!  750 char max")
     private String description;
 
-    public Event(String name, String description) {
-        this.name = name;
-        this.description = description;
+    public Event(){
         this.id = nextId;
         nextId++;
+    }
+
+    public Event(String name, String contactEmail, boolean required, Integer attendees, String location, String description){
+        this();
+        this.name = name;
+        this.contactEmail = contactEmail;
+        this.required = required;
+        this.attendees = attendees;
+        this.location = location;
+        this.description = description;
+    }
+
+
+
+    public Integer getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(Integer attendees) {
+        this.attendees = attendees;
+    }
+
+    public Boolean getRequired() {
+        return required;
+    }
+
+    public void setRequired(Boolean required) {
+        this.required = required;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
     }
 
     public String getName() {
@@ -27,6 +80,14 @@ public class Event {
 
     public String getDescription() {
         return description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public void setDescription(String description) {
